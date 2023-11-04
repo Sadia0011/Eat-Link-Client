3d885f194a4d1ac52a1d52f482e03a724ea1cf50
sadia sultana <sadiamoon2019@gmail.com>
2023-11-05 03:21:05 +0600

    added router

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,45 +1,58 @@
 import React from 'react';
-
+import { NavLink } from 'react-router-dom';
+import "./Navbar.css"
 const Navbar = () => {
+  const navLinks=<>
+  
+  
+  </>
     return (
-        <div className="navbar bg-base-100">
-  <div className="navbar-start">
+        <div className="navbar bg-base-100 sticky">
+<div className='max-w-7xl flex  mx-auto'>
+<div className="navbar-start">
     <div className="dropdown">
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
+        <li id='sidebar'><NavLink to="/" >Home</NavLink></li>
+        <li id='sidebar'><NavLink to="/allFoodItems">All Food Items</NavLink></li>
+        <li id='sidebar'><NavLink to="/blog">Blog</NavLink></li>
         <li>
-          <a>Parent</a>
+          <a>User</a>
           <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
+            <li id='sidebar'><NavLink to="/addedFood">My Added Food Items</NavLink></li>
+            <li id='sidebar'><NavLink to="/add">Add a  Food Items</NavLink></li>
+            <li id='sidebar'><NavLink to="/orderedFood">My Ordered Food Items</NavLink></li>
+            
           </ul>
         </li>
-        <li><a>Item 3</a></li>
+        <li id='sidebar'><NavLink to="/login">Login</NavLink></li>
       </ul>
     </div>
     <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
+        <li id='sidebar'><NavLink to="/" >Home</NavLink></li>
+        <li id='sidebar'><NavLink to="/allFoodItems">All Food Items</NavLink></li>
+        <li id='sidebar'><NavLink to="/blog">Blog</NavLink></li>
       <li tabIndex={0}>
         <details>
-          <summary>Parent</summary>
+          <summary>User</summary>
           <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
+            <li id='sidebar'><NavLink to="/addedFood">My Added Food Items</NavLink></li>
+            <li id='sidebar'><NavLink to="/add">Add a  Food Items</NavLink></li>
+            <li id='sidebar'><NavLink to="/orderedFood">My Ordered Food Items</NavLink></li>
+            
           </ul>
         </details>
       </li>
-      <li><a>Item 3</a></li>
+     
+      <li id='sidebar'><NavLink to="/login">Login</NavLink></li>
     </ul>
   </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
-  </div>
+</div>
 </div>
     );
 };
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const ErrorPage = () => {
+    const navigate=useNavigate()
+    const handleHome=()=>{
+        navigate("/")
+    }
     return (
-        <div className='flex flex-col justify-center items-center'>
-            <div>
-                <img src="https://i.ibb.co/hXSTJws/error.png" alt="" />
+        <div className=' min-h-screen flex gap-5  flex-col justify-center items-center'>
+            <div className=''>
+                <img 
+                className='w-[600px] h-[300px] object-cover'
+                src="https://i.ibb.co/hXSTJws/error.png" alt="" />
             </div>
             <div>
-                <button className='btn btn-primary bg-orange-400 font-bold'>Home</button>
+                <button
+                onClick={handleHome}
+                className='btn btn-primary bg-orange-400 font-bold'>Home</button>
             </div>
         </div>
     );
