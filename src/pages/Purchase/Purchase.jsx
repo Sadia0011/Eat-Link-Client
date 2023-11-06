5a062eaad23c8a61955db9afef091c60a8125eca
sadia sultana <sadiamoon2019@gmail.com>
2023-11-06 20:32:37 +0600

    stored data in db from purchase button

--- src/pages/Purchase/Purchase.jsx
+++ src/pages/Purchase/Purchase.jsx
@@ -1,12 +1,62 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useLoaderData, useNavigate } from 'react-router-dom';
 import useAuth from '../../Hooks/useAuth';
-
+import Swal from 'sweetalert2'
 const Purchase = () => {
     const food=useLoaderData()
     const {food_name,img,category,price,quantity,made_by,food_origin,description}=food
     const navigate=useNavigate()
     const {user}=useAuth()
+    const email=user.email
+    const purchaseProduct={
+      food_name,
+      img,
+      category,
+      price,
+      quantity,
+      email
+    }  
+    console.log(purchaseProduct)
+
+    const handlePurchase=()=>{
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You want to purchase it !.",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, add it!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          fetch(
+            "http://localhost:5000/orderedItem",{
+              method: "POST",
+              headers: {
+                "content-type": "application/json",
+              },
+              body: JSON.stringify(purchaseProduct),
+            }
+          )
+            .then((res) => res.json())
+            .then((data) => {
+              console.log(data);
+              if ((data.insertedId = 1)) {
+                Swal.fire({
+                  icon: "success",
+                  title: "Added Food Successfully",
+                  text: "Congratulations!",
+                  confirmButtonText: "Cool",
+                });
+              }
+            });
+
+      
+        }
+      });
+    }
+
+
     return (
         <div>
         <h2 className="text-3xl italic font-semibold text-center my-5">{food_name}</h2>
@@ -24,7 +74,7 @@ const Purchase = () => {
         <h3 className="text-xl"><span className='font-bold'>Price :</span> ${price}</h3>
         <h3 className="text-xl"><span className='font-bold'>Quantity :</span> {quantity}</h3>
        <div className='flex flex-col lg:flex-row gap-3 justify-center items-center'>
-       <button className='btn bg-orange-400 text-white'>Purchase</button>
+       <button onClick={handlePurchase} className='btn bg-orange-400 text-white'>Purchase</button>
        <button
        onClick={()=>{navigate(-1)}}
        className='btn bg-orange-400 text-white'>Back</button>
